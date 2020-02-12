bc46dc04fe957acfec59be265a9b81eaa658cf56
anion0278 <anion0278@gmail.com>
2020-02-12 12:38:53 +0100

    Some fixes in green tape recognizer

--- HGR_CNN/simple_recognizer.py
+++ HGR_CNN/simple_recognizer.py
@@ -1,28 +1,44 @@
+import cv2
+import numpy as np
+
 def recognize_finger_tip(color_image, depth_image):
     
     #green tape
-    upper = (140,150,30)
+    upper = (140,150,30)  
     lower = (70,74,0)
 
     mask = cv2.inRange(color_image, lower, upper)
-    #_,contours, hierarchy_ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-    contours,_ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+    cv2.imshow('ImageWindow', mask)
+    cv2.waitKey(1)
+
+    try:
+        _, contours, hierarchy_ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+    except:
+        contours,_ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
     try:
         if not contours:
             print('Contour not detected')
+            return (0,0,0), False
+
         else:
             blob = max(contours, key=lambda el: cv2.contourArea(el))
+                
+            #minRect = cv2.minAreaRect(blob)
+            if len(blob) < 70:
+                return (0,0,0), False
+
             M = cv2.moments(blob)
             center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
-            
-            minRect = cv2.minAreaRect(blob)
-            minRectPoints = cv2.boxPoints(minRect)
-            depths = depth_image[minRectPoints[0],minRectPoints[1],minRectPoints[2],minRectPoints[3]]
 
-            Z = np.median(depths)
+            z_heights = []
+            for point in blob:
+                point_a = point[0]
+                z_heights.append(depth_image[point_a[0], point_a[1]])
+            Z = np.round(np.mean(z_heights),0)
+
             return (center[0],center[1],Z), True
     
-    finally:
+    except Exception as ex:
         print('No blob detected')
-        return False
+        return (0,0,0), False
     
