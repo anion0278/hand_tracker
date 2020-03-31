e6929ceb6d1b29cc7fd524fe29d2fa9860019607
Vysocky Ales <alivy@seznam.cz>
2020-03-31 15:44:20 +0200

    blob_recognizer - opencv fix

--- HGR_CNN/blob_recognizer.py
+++ HGR_CNN/blob_recognizer.py
@@ -1,12 +1,13 @@
 import cv2
 
 def find_blob(image):
+    center = None
     try:
         _, contours, _ = cv2.findContours(image, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
     except:
         contours,_ = cv2.findContours(image.astype("uint8"), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
     if len(contours) == 0:
-        return None
+        return center
     largest_blob = max(contours, key=lambda element: cv2.contourArea(element))
     M = cv2.moments(largest_blob)
     center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
